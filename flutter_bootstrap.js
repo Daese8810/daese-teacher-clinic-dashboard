cc91387e537ff5c496dcf293c18649c43efc48cc
Daese8810 <ltdall0313@gmail.com>
2026-04-26 15:52:47 +0900

    Update clinic table layout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1256788906" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "746893231" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
