4202c0c7dadeab16756ba759adf5f6fbd9d37533
Daese8810 <ltdall0313@gmail.com>
2026-05-03 14:23:36 +0900

    Deploy first button grammar usage scope

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1505134984" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3004620406" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
