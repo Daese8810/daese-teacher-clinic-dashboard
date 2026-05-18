692def05780a3521c84b3f7a0b94f8776fdfb2ca
Daese8810 <ltdall0313@gmail.com>
2026-05-18 20:34:22 +0900

    Collapse roster student lists by default

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1981648495" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2890005979" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
