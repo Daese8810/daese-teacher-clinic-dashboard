e32cba4fc81681db7923d5190747895fbb31b5a6
Daese8810 <ltdall0313@gmail.com>
2026-05-22 22:08:33 +0900

    Deploy exam score analysis improvements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2904978314" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3472217687" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
