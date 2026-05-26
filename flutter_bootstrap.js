21d90a7c268443675fa149bc6ce41b02a18e92e0
Daese8810 <ltdall0313@gmail.com>
2026-05-26 18:39:26 +0900

    Deploy monthly report professional A4 summary

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "751482913" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3171785672" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
