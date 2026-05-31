e6ffe5a589e6d8c82e76e2f1ec3086ef7df70846
Daese8810 <ltdall0313@gmail.com>
2026-05-31 19:39:29 +0900

    Deploy rehearsal type sample profile UI

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2802099791" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1227283860" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
