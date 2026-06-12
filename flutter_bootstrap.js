2e58c68bac54eed3c69c49b26566affc6587ee83
Daese8810 <ltdall0313@gmail.com>
2026-06-12 19:12:33 +0900

    Deploy alimtalk monthly summary

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1965856972" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1409503019" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
