5adae51a278f8ae3d08a8c80c14758e314507778
Daese8810 <ltdall0313@gmail.com>
2026-06-16 23:23:57 +0900

    Deploy clinic table slim initial load

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"77e2e94772b6eb43759e34ed1ad7da4674e19c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "194643779" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "214535564" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
